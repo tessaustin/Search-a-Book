ce52f70420245ac5ff5d6a00b77aba687d678fea
Tess Austin <tessaustin92@gmail.com>
2021-06-14 12:06:42 -0600

    addUser and login mutations added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,6 +14,33 @@ const resolvers = {
 
             throw new AuthenticationError('Not logged in');
         }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        
+        login: async (parent, {email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
     }
 };
 
