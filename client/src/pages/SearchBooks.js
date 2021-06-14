9b21138a2f1e53c18e0b5b4bed376c284847b555
Tess Austin <tessaustin92@gmail.com>
2021-06-14 15:36:35 -0600

    apollo hook implemented

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,6 +5,9 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+import { useMutation } from '@apollo/react-hooks';
+import { SAVE_BOOK } from '../utils/mutations';
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -64,12 +67,14 @@ const SearchBooks = () => {
       return false;
     }
 
-    try {
-      const response = await saveBook(bookToSave, token);
+      try {
+        const {data} = await saveBook({
+          variables: { input: bookToSave }
+        });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+        if (error) {
+          throw new Error('Oops! something went wrong!');
+        }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
