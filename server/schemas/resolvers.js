0c46987011d85b8b40380a9c846d51cb8e372462
Tess Austin <tessaustin92@gmail.com>
2021-06-14 12:18:58 -0600

    resolvers added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,8 +23,8 @@ const resolvers = {
 
             return { token, user };
         },
-        
-        login: async (parent, {email, password }) => {
+
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
             if (!user) {
@@ -33,7 +33,7 @@ const resolvers = {
 
             const correctPw = await user.isCorrectPassword(password);
 
-            if(!correctPw) {
+            if (!correctPw) {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
@@ -41,6 +41,29 @@ const resolvers = {
             return { token, user };
         },
 
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Need to be logged in!')
+        },
+
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Need to be logged in!')
+        }
     }
 };
 
